09071564a5cf5e59036be196725e07c91865949a
Aloisius <kalosius43@gmail.com>
2023-06-24 17:26:22 +0300

    final product

--- kalimunda/settings.py
+++ kalimunda/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'dawa',
     'django_filters',
     'pharmacies',
-    'whitenoise',
+    # 'whitenoise',
     
 ]
 
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 
 
 ]
@@ -159,11 +159,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-MEDIA_URL = 'images/'  
+# STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# MEDIA_URL = 'images/'  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
