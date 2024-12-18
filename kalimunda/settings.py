e41432d3ba7cec7bd0211dcd33464fe976e7aa1c
kalosius <kalosius43@gmail.com>
2024-12-18 13:30:23 +0300

    collected static

--- kalimunda/settings.py
+++ kalimunda/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/')
+# ]
 # MEDIA_URL = 'images/'  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
